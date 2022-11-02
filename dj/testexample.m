a4030938e62b6a119184a04431d134820d9d82e4
chrisvdt <c.vandertogt@nin.knaw.nl>
2022-11-02 17:02:37 +0100

    Update testexample.m

--- dj/testexample.m
+++ dj/testexample.m
@@ -1,8 +1,5 @@
 %% query and Import records from FYD 
-%a simply way to retrieve urls to access the data
-urls = getSessions(project='SomeProject', subject='aSubject');
-
-%% Making a spreadsheet that can be updated as new json files are added to the server
+% Making a spreadsheet that can be updated as new json files are added to the server
 %first add the datajoint toolbox to matlab from matlab addons
 setenvdj    %set environment credentials for MySql database
 populate(shared.Example) %update table of records with specific characteristics
