9b8902cd436dfd0938a9caee8308c4d2b0f07d79
Chris van der Togt <c.vandertogt@nin.knaw.nl>
2023-04-05 19:05:19 +0200

    add initialze fydsaved path

--- getFYD.m
+++ getFYD.m
@@ -11,7 +11,6 @@ function fields = getFYD()
 %You cannot easily export data from a mlapp, but you can pass it a class 
 %handle to store the data you need after the mlapp is deleted 
     HStore = Fydflds(); %Class handle store
-    app = getFYDfields(HStore); %mlapp GUI to select input values
-    waitfor(app) 
+    waitfor(getFYDfields(HStore)) %mlapp GUI to select input values
     fields = HStore.fields; %return values
     
