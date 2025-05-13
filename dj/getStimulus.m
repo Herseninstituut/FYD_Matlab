6df03dcea022a8c872b295162f0e89f2153a766c
chrisvdt <c.vandertogt@nin.knaw.nl>
2025-05-13 14:44:38 +0200

    added email to getFYD

--- dj/getStimulus.m
+++ dj/getStimulus.m
@@ -10,11 +10,16 @@ Database = dbpar.Database;  %= yourlab
 query = eval([Database '.Stimulus']);
 stimSel = ['stimulusid= "' stim '"'];
 
-md = fetch(query & stimSel, 'stimulusid', 'shortdescr');
+md = fetch(query & stimSel, 'stimulusid', 'shortdescr', 'longdescr');
 source = '';
 if isfile(which(md.stimulusid))
     source = fileread(which(md.stimulusid));
 else
     disp("Path to stimulus presentation mfile is required!!")
 end
-task_metadata = struct( 'task_name', md.stimulusid, 'task_description', md.shortdescr, 'source', source );
+
+if isempty(md.longdescr)
+    disp("Please add a task description!!!")
+end
+
+task_metadata = struct( 'task_name', md.stimulusid, 'task_shortdescr', md.shortdescr, 'task_description', md.longdescr, 'source', source );
