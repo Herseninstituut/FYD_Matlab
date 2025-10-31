455afe9acdf8fc9204afb5b8d8f6bf2dffcade3b
chrisvdt <c.vandertogt@nin.knaw.nl>
2025-10-31 17:48:41 +0100

    Update getSessions.m

--- dj/getSessions.m
+++ dj/getSessions.m
@@ -1,5 +1,8 @@
 function Sessions = getSessions(varargin)
 % urls = getSessions(project='someProject', subject='aSubject')
+% valid entries;
+% sessionid, project, dataset, condition, subject, stimulus, setup, investigator
+% date as '< date', '> date' or 'date - date'
 
 %Database access with Datajoint
 global dbpar
@@ -8,7 +11,7 @@ global dbpar
  addOptional(p,'sessionid','-',@(x)validateattributes(x,{'char'},{'nonempty'}))
  addOptional(p,'project','-',@(x)validateattributes(x,{'char'},{'nonempty'}))
  addOptional(p,'dataset','-',@(x)validateattributes(x,{'char'},{'nonempty'}))
- addOptional(p,'excond','-',@(x)validateattributes(x,{'char'},{'nonempty'}))
+ addOptional(p,'condition','-',@(x)validateattributes(x,{'char'},{'nonempty'}))
  addOptional(p,'subject','-',@(x)validateattributes(x,{'char'},{'nonempty'}))
  addOptional(p,'stimulus','-',@(x)validateattributes(x,{'char'},{'nonempty'}))
  addOptional(p,'setup','-',@(x)validateattributes(x,{'char'},{'nonempty'}))
@@ -26,17 +29,39 @@ else
    for i = 1:length(flds)
        fld = flds{i};
        val = Ins.(fld);
+       if strcmp(fld, 'investigator') %in database this is investid
+           fld = 'investid';
+       elseif strcmp(fld, 'condition')
+           fld = 'excond';
+       end                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
         if ~strcmp(val, '-')
-            if isempty(strSel)
-                strSel = [fld '="' val '"'];
+            if strcmp(fld, 'date')
+                resa = regexp(val, '^[<>] ', 'match');
+                resb = regexp(val, '\d{4}-\d{2}-\d{2} +- +\d{4}-\d{2}-\d{2}', 'match');
+                if ~isempty(resa)
+                    val = erase(val, resa{1});
+                    whereclause = [fld ' ' resa{1} '"' val '"'];
+                elseif ~isempty(resb)
+                    val = split(resb{1}, ' - ');
+                    val = strip(val);
+                    whereclause = [fld ' > "' val{1} '" AND ' fld ' < "' val{2} '"'];
+                else
+                    whereclause = [fld '="' val '"'];
+                end
             else
-                strSel = [strSel ' AND ' fld '="' val '"'];
+                whereclause = [fld '="' val '"'];
+            end
+            
+            if isempty(strSel)
+                strSel = whereclause;
+            else
+                strSel = [strSel ' AND ' whereclause];
             end
         end
    end
 end
 
-if isempty(strSel)
+if isempty(strSel)                                                                                                                                                                                                                                                                      
     return
 end
 
@@ -64,11 +89,11 @@ end
 end
 
 function url = geturl(obj)
-    switch obj.server
-        case {'VS03', 'VS01'}
-            url = fullfile(strrep(obj.url, 'mnt', ''));
-            
-        otherwise
-            url = fullfile(strrep(obj.url, './', ['\\' obj.server '\']));
-    end
+   switch obj.server
+       case {'VS03', 'VS01', 'mnt'}
+            url = fullfile(erase(obj.url, 'mnt'));
+
+       otherwise
+           url = fullfile(strrep(obj.url, './', ['\\' obj.server '\']));
+   end
 end
